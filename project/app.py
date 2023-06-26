a8593f3c59e321bcdc60c257d0cee94dc141437f
cedrick <cedrickchu123@gmail.com>
2023-06-26 15:41:20 +0800

    adding os.environ

--- project/app.py
+++ project/app.py
@@ -1,26 +1,17 @@
 from flask import Flask, render_template, current_app
 from cryptography.fernet import Fernet
-from config import Config
+from os
 import secrets
 import requests
 
 app = Flask(__name__)
-
-app.config.from_object('config.Config')
-
-def decrypt_token(encrypted_token, encryption_key):
-    cipher_suite = Fernet(encryption_key)
-    decrypted_token = cipher_suite.decrypt(encrypted_token.encode())
-    return decrypted_token.decode()
 
 def inject_nonce():
     return {'nonce': secrets.token_hex(16)}
 
 @app.route("/")
 def home():
-    encrypted_token = current_app.config['FACEBOOK_ACCESS_TOKEN']
-    encryption_key = current_app.config['SECRET_KEY']
-    access_token = decrypt_token(encrypted_token, encryption_key)
+    access_token = os.environ['FACEBOOK_ACCESS_TOKEN']
     latest_post_url = fetch_facebook_posts(access_token)
     return render_template('index.html', latest_post_url=latest_post_url, nonce=inject_nonce())
 
