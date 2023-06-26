13231356bc1fc0b680e5cc5b8f72186cd7ffad0e
cedrick <cedrickchu123@gmail.com>
2023-06-26 18:25:29 +0800

    aaa

--- project/app.py
+++ project/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, current_app
+from flask import Flask, render_template
 import os
 import secrets
 import requests
 import logging
+
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
+
+
 def inject_nonce():
     return {'nonce': secrets.token_hex(16)}
+
 
 @app.route("/")
 def home():
@@ -15,24 +19,41 @@ def home():
     latest_post_url = fetch_facebook_posts(access_token)
     return render_template('index.html', latest_post_url=latest_post_url, nonce=inject_nonce())
 
+
 def fetch_facebook_posts(access_token):
     page_id = '108136152254757'
     limit = 5
 
-    # API request to retrieve the latest posts
-    url = f'https://graph.facebook.com/{page_id}/posts?access_token={access_token}&limit={limit}&fields=permalink_url'
-    response = requests.get(url)
-    if response.status_code == 200:
+    url = f'https://graph.facebook.com/{page_id}/posts'
+    params = {
+        'access_token': access_token,
+        'limit': limit,
+        'fields': 'permalink_url'
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+
         data = response.json()
         latest_post_url = data['data'][0]['permalink_url']
-    else:
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"An error occurred: {e}")
+        latest_post_url = None
+
+    except (KeyError, IndexError) as e:
+        logging.error("Invalid or empty response from the Facebook API.")
+        logging.error(f"Error details: {e}")
         latest_post_url = None
 
     return latest_post_url
 
+
 @app.route("/more")
 def learnmore():
     return render_template('learnMore.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
