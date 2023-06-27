ff737a478484d03aa3891ca8a979f4baf8e6e97e
cedrick <cedrickchu123@gmail.com>
2023-06-27 15:24:41 +0800

    Style changes

--- project/app.py
+++ project/app.py
@@ -3,6 +3,7 @@ import os
 import secrets
 import requests
 import logging
+from datetime import datetime, timedelta
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
@@ -11,24 +12,47 @@ def inject_nonce():
 
 @app.route("/")
 def home():
-    access_token = os.environ['FACEBOOK_ACCESS_TOKEN']
-    latest_post_url = fetch_facebook_posts(access_token)
-    return render_template('index.html', latest_post_url=latest_post_url, nonce=inject_nonce())
+    access_token = ['FACEBOOK_ACCESS_TOKEN']
+    latest_post_urls = fetch_facebook_posts(access_token)
+    return render_template('index.html', latest_post_urls=latest_post_urls, nonce=inject_nonce())
 
-def fetch_facebook_posts(access_token):
+
+def fetch_facebook_posts(access_token, hashtag='#TicketsOnSale', days=30):
     page_id = '108136152254757'
     limit = 5
 
-    # API request to retrieve the latest posts
-    url = f'https://graph.facebook.com/{page_id}/posts?access_token={access_token}&limit={limit}&fields=permalink_url'
+    # Calculate the date range for fetching posts
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=days)
+
+    # API request to retrieve the posts within the date range
+    url = f'https://graph.facebook.com/{page_id}/posts?access_token={access_token}&limit={limit}&fields=permalink_url,message,created_time'
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        latest_post_url = data['data'][0]['permalink_url']
+        if 'data' in data:
+            posts = data['data']
+            latest_posts = []
+            for post in posts:
+                message = post.get('message', '')
+                created_time = post.get('created_time')
+                post_date = datetime.strptime(created_time, "%Y-%m-%dT%H:%M:%S+0000")
+                if start_date <= post_date <= end_date and hashtag in message:
+                    post_data = {
+                        'permalink_url': post['permalink_url'],
+                        'created_time': post_date
+                    }
+                    latest_posts.append(post_data)
+            latest_posts = sorted(latest_posts, key=lambda x: x['created_time'], reverse=True)
+            latest_post_urls = [post['permalink_url'] for post in latest_posts]
+        else:
+            latest_post_urls = []
     else:
-        latest_post_url = None
+        latest_post_urls = []
 
-    return latest_post_url
+    return latest_post_urls[:limit]
+
+
 
 @app.route("/more")
 def learnmore():
