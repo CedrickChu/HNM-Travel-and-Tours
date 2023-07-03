71733733cdf02d41a0cd1f1d401bbcaf54448e7e
cedrick <cedrickchu123@gmail.com>
2023-07-03 16:32:00 +0800

    aa

--- project/app.py
+++ project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, current_app
+from flask import Flask, render_template, current_app, jsonify
 import os
 import secrets
 import requests
@@ -6,17 +6,44 @@ import logging
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
-
+gallery = [
+        {
+            'index': 1,
+            'id': 'Attraction1',
+            'class=' : 'mySlides1',
+            'heading': "1.) Underground River Tour",
+            'description': "Puerto Princesa City is the center of Palawan and best known for the internationally recognized Puerto Princesa Subterranean River (or Underground River), a UNESCO World Heritage Site and one of the New 7 Wonders of Nature. Not only that, the city is critically acclaimed for its environmental excellence.",
+            'image': ["/static/images/underground.jpg", "/static/images/underground2.jpg", "/static/images/underground3.jpg"]
+        },
+        {
+            'index': 2,
+            'id': 'Attraction2',
+            'class=' : 'mySlides2',
+            'heading': "2.) El Nido Island Hopping",
+            'description': "El Nido, located on Palawan island in the Philippines, is recognized for its pristine beaches and vibrant coral reefs. Despite the 5-6 hour drive from the town center, the journey is worthwhile due to the island's abundant natural beauty and awaiting ecosystem.",
+            'image': ["/static/images/elnido.jpg", "/static/images/elnido-beach.jpg", "/static/images/elnidoBeach.jpg"]
+        },
+        {
+            'index': 3,
+            'id': 'Attraction3',
+            'class=' : 'mySlides3',
+            'heading': "3.) Culion Island Tour",
+            'description': "qqqqqqqqqqqqqqqq",
+            'image': ["/static/images/culionIsland2.jpg", "/static/images/culionIsland.jpg", "/static/images/Culion-island.jpg"]
+        }
+    ]
 logging.basicConfig(level=logging.DEBUG)
 
 def inject_nonce():
     return {'nonce': secrets.token_hex(16)}
 
+
 @app.route("/")
 def home():
-    access_token = ['FACEBOOK_ACCESS_TOKEN']
+    access_token = os.environ['FACEBOOK_ACCESS_TOKEN']
     latest_post_urls = fetch_facebook_posts(access_token)
-    return render_template('index.html', latest_post_urls=latest_post_urls, nonce=inject_nonce())
+    return render_template('index.html', latest_post_urls=latest_post_urls, gallery=gallery,nonce=inject_nonce())
+
 
 
 def fetch_facebook_posts(access_token, hashtag='#TicketsOnSale', days=30):
@@ -54,15 +81,18 @@ def fetch_facebook_posts(access_token, hashtag='#TicketsOnSale', days=30):
 
     return latest_post_urls[:limit]
 
+@app.route("/api/gallery")
+def carousel():
+    return jsonify(gallery)
 
+@app.route("/test")
+def test():
+    return render_template('test.html', gallery=gallery)
 
 @app.route("/more")
 def learnmore():
   return render_template('learnMore.html')
 
-@app.route("/test")
-def test():
-    return render_template('test.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
